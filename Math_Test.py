d7c531ace44baf1069505ab228c0c66719af0c91
Štěpán Hodboď <hodbods10@gmail.com>
2025-02-25 10:34:48 +0100

    Update Math_Test.py
    adding colors

--- Math_Test.py
+++ Math_Test.py
@@ -1,7 +1,7 @@
 import random
 
 print("WELCOME TO EASY MATH TEST!")
-rn = 7
+rn = 0
 while rn > -1 and rn <= 9:  
     random_number1 = random.randint(1,10)
     random_number2 = random.randint(1,10)
@@ -12,15 +12,15 @@ while rn > -1 and rn <= 9:
 
     if ra1 == ya1:
         rn = rn + 1
-        print("yes!")
+        print("\033[32mYES!!\033[0m")
         print (f"your score is {rn}")
     else:
-        print ("no!")
+        print("\033[31mNOO!!\033[0m")
         rn = rn - 1
         print (f"your score is {rn}")
 
 
 if rn == -1:
-    print("you lost!!")
+    print("\033[31myou lost!!\033[0m")
 else:
-    print ("you won!")
+    print("\033[32myou won!\033[0m")
