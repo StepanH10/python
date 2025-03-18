958b96d2be3a37089e2c241a6cceefa2510c3363
Štěpán Hodboď <hodbods10@gmail.com>
2025-03-18 11:21:18 +0100

    update guess the numer 2p

--- guess nuber 2p .py
+++ guess nuber 2p .py
@@ -1,37 +1,44 @@
-win = 1
-x = 0
+
 p1_score = 0
 p2_score = 0
 
-player_1 = int(input("select number 1-10 \n"))
+player_1_name = input("player 1 name \n")
+
+player_2_name = input("player 2 name \n")
+
+win = int(input("How many rounds? \n"))
+
+player_1 = int(input(f"{player_1_name} select number 1-10 \n"))
 if player_1 >= 11:
     print ("numer is too high")
+    player_1 = int(input(f"{player_1_name} select number 1-10 \n"))
 
-player_2 = int(input("select numer 1-10 \n"))
+player_2 = int(input(f"{player_2_name} select numer 1-10 \n"))
 if player_2 >= 11:
     print ("numer is too high")
+    player_2 = int(input(f"{player_2_name} select numer 1-10 \n"))
 
-
-while x == 0:
-    p1_guess = int(input("p1 guess \n"))
+while True:
+    p1_guess = int(input(f"{player_1_name} guess \n"))
     if p1_guess == player_2:
         p1_score = p1_score + 1
-        print (f"yes your score is {p1_score}")
+        print (f"\033[32myes your score is {p1_score}\033[0m")
+        player_2 = int(input(f"{player_2_name} select new  numer 1-10 \n"))
     else:
         print ("wrong!")
     if p1_score == win:
         break
-    p2_guess = int(input("p2 guess \n"))
+    p2_guess = int(input(f"{player_2_name} guess \n"))
     if p2_guess == player_1:
         p2_score = p2_score + 1
-        print (f"yes your score is  {p2_score}")
+        print (f"\033[32myes your score is  {p2_score}\033[0m")
+        player_1 = int(input(f"{player_1_name} select new numer 1-10 \n"))
     else:
         print ("wrong")
     if p2_score == win:
         break
 
-
 if p1_score == win:
-    print ("player 1 won")
+    print (f"{player_1_name} won!")
 else:
-    print ("player 2 won")
+    print (f"{player_2_name} won!")
