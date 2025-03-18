37fa63ccce9b5f76ccbc0376a265ef0aeb839a57
Štěpán Hodboď <hodbods10@gmail.com>
2025-03-18 21:52:01 +0100

    update

--- guess_number_2p .py
+++ guess_number_2p .py
@@ -1,3 +1,5 @@
+import sys
+import string
 
 p1_score = 0
 p2_score = 0
@@ -9,11 +11,16 @@ player_2_name = input("player 2 name \n")
 win = int(input("How many rounds? \n"))
 
 player_1 = int(input(f"{player_1_name} select number 1-10 \n"))
+sys.stdout.write("\033[F")
+sys.stdout.write("\033[K")
 if player_1 >= 11:
     print ("numer is too high")
     player_1 = int(input(f"{player_1_name} select number 1-10 \n"))
 
 player_2 = int(input(f"{player_2_name} select numer 1-10 \n"))
+sys.stdout.write("\033[F")
+sys.stdout.write("\033[K")
+
 if player_2 >= 11:
     print ("numer is too high")
     player_2 = int(input(f"{player_2_name} select numer 1-10 \n"))
@@ -24,6 +31,8 @@ while True:
         p1_score = p1_score + 1
         print (f"\033[32myes your score is {p1_score}\033[0m")
         player_2 = int(input(f"{player_2_name} select new  numer 1-10 \n"))
+        sys.stdout.write("\033[F")
+        sys.stdout.write("\033[K")
     else:
         print ("wrong!")
     if p1_score == win:
@@ -33,8 +42,10 @@ while True:
         p2_score = p2_score + 1
         print (f"\033[32myes your score is  {p2_score}\033[0m")
         player_1 = int(input(f"{player_1_name} select new numer 1-10 \n"))
+        sys.stdout.write("\033[F")
+        sys.stdout.write("\033[K")
     else:
-        print ("\033[31mwrong\033[0m")
+        print ("wrong")
     if p2_score == win:
         break
 
