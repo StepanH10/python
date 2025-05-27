043ae53e2a921e64a475f620419c7feacac5ec05
Štěpán Hodboď <hodbods10@gmail.com>
2025-05-27 08:36:24 +0200

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+options = {
+    "p1_name": "george",
+    "p2_name": "sally",
+    "difficulty" : 2,
+}
+print (options)
+
+
+options["p1_name"] = input("Enter player 1 name: ")
+options["p2_name"] = input("Enter player 2 name: ")
+print (options)
